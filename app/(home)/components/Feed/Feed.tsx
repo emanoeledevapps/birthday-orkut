fa558d629dc72d106af215b197d568ef944a3d8b
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-09-18 10:26:27 -0300

    feat: add pagination on feed

--- app/(home)/components/Feed/Feed.tsx
+++ app/(home)/components/Feed/Feed.tsx
@@ -5,13 +5,14 @@ import { useEffect, useRef, useState } from "react";
 import { Post as PostProps } from "@/app/generated/prisma";
 import { getPosts } from "@/app/actions";
 import { Post } from "@/components/Post/Post";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export function Feed() {
   const [posts, setPosts] = useState<PostProps[]>([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-  const loader = useRef(null);
+  const listEnd = useRef(null);
 
   async function fetchPosts(pageNumber: number) {
     setLoading(true);
@@ -33,11 +34,54 @@ export function Feed() {
     fetchPosts(page);
   }, []);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !loading && hasMore) {
+          setPage((prev) => prev + 1);
+        }
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    if (listEnd.current) {
+      observer.observe(listEnd.current);
+    }
+
+    return () => {
+      if (listEnd.current) observer.unobserve(listEnd.current);
+    };
+  }, [loading, hasMore]);
+
+  useEffect(() => {
+    if (page === 1) return;
+    fetchPosts(page);
+  }, [page]);
+
+  if (loading && posts.length === 0) {
+    return (
+      <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+        <AiOutlineLoading3Quarters
+          size={30}
+          className="text-blue-primary animate-spin"
+        />
+        <p className="text-blue-primary font-semibold">Carregando...</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center justify-between w-full">
+        <p className="text-text-secondary text-xs">Depoimentos</p>
+      </div>
       {posts.map((item) => (
         <Post post={item} key={item.id} />
       ))}
+
+      <div ref={listEnd} />
     </div>
   );
 }
