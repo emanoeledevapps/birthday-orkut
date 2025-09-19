0d048cd36f70771ffc3553562871799d893daf5a
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-09-19 11:14:22 -0300

    feat: add logo on footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
+import LogoImg from "@/public/assets/images/logo.png";
+
 export function Footer() {
   return (
-    <footer>
-      <p>Teste.com</p>
+    <footer className="flex items-center justify-center">
+      <Image width={140} height={50} alt="Logo" src={LogoImg} />
     </footer>
   );
 }
